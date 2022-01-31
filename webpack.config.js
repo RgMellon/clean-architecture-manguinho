44d6fea250c3fecd0a6e57823a7396f4dc37012e
Renan Melo <renanmelo@MacBook-Air-de-Renan.local>
2022-01-31 11:19:49 -0300

    style: create login styles

--- webpack.config.js
+++ webpack.config.js
@@ -3,54 +3,58 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  entry: './src/main/index.tsx',
+  mode: "development",
+  entry: "./src/main/index.tsx",
   output: {
-    path: path.join(__dirname, 'public/js'),
-    publicPath: '/public/js',
-    filename: 'bundle.js'
+    path: path.join(__dirname, "public/js"),
+    publicPath: "/public/js",
+    filename: "bundle.js",
   },
   resolve: {
-    extension: ['.ts', '.tsx', '.js', 'scss'],
+    extensions: [".ts", ".tsx", ".js", "scss"],
     alias: {
-      '@': path.join(__dirname, 'src')
-    }
+      "@": path.join(__dirname, "src"),
+    },
   },
   module: {
     rules: [
       {
         test: /\.ts(x?)$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
+        loader: "ts-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.scss$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: "style-loader",
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-              module: true
-            }
+              modules: true,
+            },
           },
           {
-            loader: 'sass-loader'
-          }
-        ]
-      }
-    ]
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
   },
 
   devServer: {
-    contentBase: './public',
-    writeToDisk: true,
-    historyApiFallback: true
+    static: {
+      directory: "./public",
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    historyApiFallback: true,
   },
   externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
+    react: "React",
+    "react-dom": "ReactDOM",
   },
-  plugins: [new CleanWebpackPlugin()]
-}
+  plugins: [new CleanWebpackPlugin()],
+};
