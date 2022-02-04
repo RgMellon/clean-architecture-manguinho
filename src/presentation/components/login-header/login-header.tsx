68bbb04b2391ab51b832b6b454d695223cf0e65d
Renan  Melo <renan.melo@wapstore.com.br>
2022-02-04 09:18:19 -0300

    refactor: move header and footer to separate files

---
+++ src/presentation/components/login-header/login-header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Logo from "../logo";
+
+import Styles from "./login-header-style.scss";
+
+const LoginHeader: React.FC = () => {
+  return (
+    <header className={Styles.header}>
+      <Logo />
+      <h1>4Dev - Enquetes para programadores</h1>
+    </header>
+  );
+};
+
+export default LoginHeader;
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -3,15 +3,14 @@ import React from "react";
 import Styles from "./login-styles.scss";
 
 import Spinner from "../../components/spinner/spinner";
-import Logo from "@/presentation/components/logo";
+
+import LoginHeader from "@/presentation/components/login-header/login-header";
+import Footer from "@/presentation/components/footer";
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <header className={Styles.header}>
-        <Logo />
-        <h1>4Dev - Enquetes para programadores</h1>
-      </header>
+      <LoginHeader />
       <form className={Styles.form}>
         <h2>Login</h2>
 
@@ -40,7 +39,7 @@ const Login: React.FC = () => {
           <span className={Styles.error}>Erro</span>
         </div>
       </form>
-      <footer className={Styles.footer}></footer>
+      <Footer />
     </div>
   );
 };
