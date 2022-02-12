173345ecdbfc5d34f66b93fb3aafe990448a4023
Renan Melo <renanmelo@MacBook-Air-de-Renan.local>
2022-02-12 19:49:27 -0300

    test: ensure login present sucess message when email  was validated

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -104,4 +104,21 @@ describe("Login Component", () => {
     expect(passwordStatus.title).toBe("Tudo certo!");
     expect(passwordStatus.textContent).toBe("🟢");
   });
+
+  test("Should show valid email if validation success", () => {
+    const { sut, validationStub } = makeSut();
+    validationStub.errorMessage = null;
+
+    const emailInput = sut.getByTestId("email");
+
+    fireEvent.input(emailInput, {
+      target: {
+        value: faker.internet.email(),
+      },
+    });
+
+    const emailStatus = sut.getByTestId("email-status");
+    expect(emailStatus.title).toBe("Tudo certo!");
+    expect(emailStatus.textContent).toBe("🟢");
+  });
 });
