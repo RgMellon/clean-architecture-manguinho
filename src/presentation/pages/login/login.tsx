07cbb3fc317104bf72fcee3257dd3ddfecd73d0e
Renan Melo <renanmelo@MacBook-Air-de-Renan.local>
2022-02-13 11:03:08 -0300

    feat: ensure login calls authentication with corret values

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -12,12 +12,14 @@ import {
 import Context from "@/presentation/contexts/form/form-context";
 
 import { Validation } from "@/presentation/protocols/validation";
+import { Authentication } from "@/domain/usecases";
 
 type Props = {
   validation: Validation;
+  authentication: Authentication;
 };
 
-const Login: React.FC<Props> = ({ validation }: Props) => {
+const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     emailError: "",
@@ -36,13 +38,15 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   }, [state.email, state.password]);
 
 
-  function handleSubmit(event: React.FormEvent<HTMLFormElement> ): void {
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement> ): Promise<void> {
     event.preventDefault();
 
     setState({
       ...state,
       isLoading: true
     })
+
+    await authentication.auth({email: state.email, password: state.password})
   }
 
   return (
