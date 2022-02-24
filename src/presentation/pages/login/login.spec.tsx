2db3157266dc8a915a7e9399aaee7a6b99d66e97
Renan Melo <renanmelo@MacBook-Air-de-Renan.local>
2022-02-23 22:13:55 -0300

    test: ensure login navigates to main page on sucess

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -29,7 +29,9 @@ type SutParams = {
   validationError?: string;
 };
 
-const history = createMemoryHistory();
+const history = createMemoryHistory({
+  initialEntries: ['/login']
+});
 
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub();
@@ -223,6 +225,8 @@ describe("Login Component", () => {
     await waitFor(() => sut.getByTestId('form'));
 
     expect(localStorage.setItem).toHaveBeenCalledWith('accessToken', authenticationSpy.account.access_token)
+    expect(history.length).toBe(1);
+    expect(history.location.pathname).toBe("/");
   });
 
   test("Should go to signup page", async () => {
