534275ce01554ae22dfd58a5fedad976c271a194
Renan Melo <renanmelo@MacBook-Air-de-Renan.local>
2022-02-26 12:27:04 -0300

    refactor: move sut creator to sut

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -4,15 +4,18 @@ import { RequiredFieldError } from "@/validation/errors/required-field-error";
 import { RequiredFieldValidation } from "./required-field-validation"
 
 
+const makeSut = (): RequiredFieldValidation =>
+  new RequiredFieldValidation(faker.random.word());
+
 describe('RequiredFieldValidation', () => {
   test('Should return error if field is empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const sut = makeSut();
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
   })
 
   test("Should return falsy if field is note empty", () => {
-    const sut = new RequiredFieldValidation("email");
+    const sut = makeSut();
     const error = sut.validate(faker.random.word());
     expect(error).toBeFalsy();
   });
