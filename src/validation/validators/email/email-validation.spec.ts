968022c93acec3b532d9962d582bcfabe4551fd8
Renan Melo <renanmelo@MacBook-Air-de-Renan.local>
2022-02-27 11:19:23 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,18 +1,22 @@
+import faker from 'faker';
+
 import { InvalidFieldError } from "@/validation/errors/invalid-field-error";
 import { FieldValidation } from "@/validation/protocols/field-validation";
 import { EmailValidation } from "./email-validation";
 
-
-
-
-
 describe('EmailValidation', () => {
   test('should return error if email is invalid', () => {
     const sut = new EmailValidation('email');
-    const error = sut.validate('');
-
-    console.log(new InvalidFieldError());
+    const error = sut.validate(faker.random.word());
 
     expect(error).toEqual(new InvalidFieldError());
   })
+
+
+   test("should return falsy if emails is valid", () => {
+     const sut = new EmailValidation("email");
+     const error = sut.validate(faker.internet.email());
+    
+     expect(error).toBeFalsy();
+   });
 })
