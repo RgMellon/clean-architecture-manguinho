76d999dc3f510046dc69e6b5c9da6bd82b5093d3
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-03-21 20:11:41 -0300

    refactor: create a make sut factory

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -2,11 +2,25 @@ import faker from 'faker'
 import { LocalSavesAccessToken } from './local-save-access-token'
 import { SetStorageSpy } from '@/data/test/mock-storage'
 
+type SutTypes = {
+  sut: LocalSavesAccessToken
+  setStorageSpy: SetStorageSpy
+}
+
+const makeSut = (): SutTypes => {
+  const setStorageSpy = new SetStorageSpy()
+  const sut = new LocalSavesAccessToken(setStorageSpy)
+
+  return {
+    sut,
+    setStorageSpy
+  }
+}
+
 describe('LocalSavesAccessToken', () => {
   test('should call SetStorage with correct value', async () => {
-    const setStorageSpy = new SetStorageSpy()
+    const { sut, setStorageSpy } = makeSut()
 
-    const sut = new LocalSavesAccessToken(setStorageSpy)
     const accessToken = faker.random.uuid()
     await sut.save(accessToken)
 
