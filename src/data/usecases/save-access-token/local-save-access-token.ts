0529983ba087cde81002296f691628a0c0f15415
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-03-21 20:02:49 -0300

    feat: ensure LocalSaveAcessToken calls SetStorage with correct value

---
+++ src/data/usecases/save-access-token/local-save-access-token.ts
@@ -0,0 +1,9 @@
+import { SetStorage } from '@/data/protocols/cache/set-storage'
+import { SaveAccessToken } from '@/domain/usecases/save-access-token'
+
+export class LocalSavesAccessToken implements SaveAccessToken {
+  constructor (private readonly setStorageSpy: SetStorage) {}
+  async save (accessToken: string): Promise<void> {
+    await this.setStorageSpy.set('accessToken', accessToken)
+  }
+}
