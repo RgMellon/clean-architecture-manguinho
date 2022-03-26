7aebb7ac7895b3e6f4c765da3521a71653f15e69
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-03-26 16:49:46 -0300

    feat: ensure RemoteAddAccount calls HttpPOstClient with correct URL

---
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -0,0 +1,15 @@
+import { HttpPostClientSpy } from '@/data/test'
+import { AccountModel } from '@/domain/models'
+import { AddAccount, AddAccountParams } from '@/domain/usecases'
+
+export class RemoteAddAccount implements AddAccount {
+  constructor (
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClientSpy<AddAccountParams, AccountModel>
+  ) {}
+
+  async add (params: AddAccountParams): Promise<AccountModel> {
+    const httpResponse = await this.httpPostClient.post({ url: this.url, body: params })
+    return null
+  }
+}
