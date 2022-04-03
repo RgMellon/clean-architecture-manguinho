e10140ba5ebdd4ea346430ff191e837273b4cda5
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-04-03 11:01:12 -0300

    test: ensure SignUp prevent submit if form is invalid

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -174,4 +174,11 @@ describe('SignUp Component', () => {
 
     expect(addAccountSpy.callsCount).toBe(1)
   })
+
+  test('Should not call AddAccount if form is invalid', async () => {
+    const validationError = faker.random.words()
+    const { sut, addAccountSpy } = makeSut({ validationError })
+    await simulateValidatedSubmit(sut)
+    expect(addAccountSpy.callsCount).toBe(0)
+  })
 })
