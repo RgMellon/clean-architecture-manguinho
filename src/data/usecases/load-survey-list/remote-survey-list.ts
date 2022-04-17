8dce6b97fbdf04836599215c85de16cd4b283b23
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-04-17 09:53:00 -0300

    feat: ensure RemoteLoadSurveyList throws Unexpected error on 403

--- src/data/usecases/load-survey-list/remote-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-survey-list.ts
@@ -1,9 +1,16 @@
+import { HttpStatusCode } from '@/data/protocols/http'
 import { HttpGetClient } from '@/data/protocols/http/http-get-client'
+import { UnexpectedError } from '@/domain/errors'
 
 export class RemoteLoadSurveyList {
   constructor (private readonly url: string, private readonly httpGetClientSpy: HttpGetClient) {}
 
   async loadAll (): Promise<void> {
-    await this.httpGetClientSpy.get({ url: this.url })
+    const httpResponse = await this.httpGetClientSpy.get({ url: this.url })
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break
+      default: throw new UnexpectedError()
+    }
   }
 }
