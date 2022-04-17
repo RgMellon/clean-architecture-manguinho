a684853330a82dd14decd7a7386dc892d099fd6b
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-04-17 10:14:47 -0300

    test: ensure RemoteLoadSurveyList returns SurveyLlist if has no error

---
+++ src/domain/tests/mock-survey-list.ts
@@ -0,0 +1,23 @@
+
+import { SurveyModel } from '../models'
+import faker from 'faker'
+
+export const mockSurveyListModel = (): SurveyModel[] => (
+  [
+    {
+      id: faker.random.uuid(),
+      question: faker.random.words(10),
+      answers: [
+        {
+          image: faker.internet.url(),
+          answer: faker.random.words(4)
+        },
+        {
+          image: faker.internet.url(),
+          answer: faker.random.words(4)
+        }
+      ],
+      didAnswer: faker.random.boolean(),
+      date: faker.date.recent()
+    }
+  ])
