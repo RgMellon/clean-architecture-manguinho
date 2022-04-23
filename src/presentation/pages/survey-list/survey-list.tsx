5e86550f9efe1a69d6676765a22fb16a2d352016
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-04-23 18:59:39 -0300

    feat: ensure SurveyList empty render 4 empty Lis on start

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Footer, Header, Icon } from '@/presentation/components'
 
 import Styles from './survey-list.styles.scss'
+import { SurveyItemEmpty } from './components'
 
 const SurveyList: React.FC = () => {
   return (
     <div className={Styles.surveyListWrap}>
       <Header />
-
+      <ul data-testid="survey-list">
+        <SurveyItemEmpty />
+      </ul>
       <Footer />
     </div>
   )
