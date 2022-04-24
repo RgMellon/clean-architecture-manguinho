0ccb6ade77f42ac35e18d9125fda18f59cf84691
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-04-24 11:45:54 -0300

    refactor: move sut to a create factory

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -4,12 +4,17 @@ import React from 'react'
 import { mockSurveyModel } from '@/domain/tests'
 import { IconName } from '@/presentation/components/icon/icon'
 
+const makeSut = (survey = mockSurveyModel()): void => {
+  render(<SurveyItem survey={survey} />)
+}
+
 describe('SurveyItem Component', () => {
   test('Should render with correct values', () => {
     const survey = mockSurveyModel()
     survey.didAnswer = true
     survey.date = new Date('2022-04-24T00:00:00')
-    render(<SurveyItem survey={survey} />)
+    makeSut(survey)
+
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
     expect(screen.getByTestId('day')).toHaveTextContent('24')
