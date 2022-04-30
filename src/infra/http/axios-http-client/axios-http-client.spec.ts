e6fbba2e02b82a93b4a700bb477f13deb9d47ed6
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-04-30 15:28:12 -0300

    test: ensure AxiosHttpClient calls axios with correct params

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -65,7 +65,7 @@ describe('AxiosHttpClient', () => {
       const { sut, mockedAxios } = makeSut()
       await sut.get(request)
 
-      expect(mockedAxios.get).toHaveBeenCalledWith(request.url)
+      expect(mockedAxios.get).toHaveBeenCalledWith(request.url, { headers: request.headers })
     })
 
     it('Should return correct response on axios.get', async () => {
