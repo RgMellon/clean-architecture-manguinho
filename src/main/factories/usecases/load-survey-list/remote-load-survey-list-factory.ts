51ce7f3559776b0d0cae111c0892224afacf18a9
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-05-01 14:26:12 -0300

    feat: add AuthorizeHtppGetClientDecorator on factory

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -1,8 +1,9 @@
 import { makeApiUrl } from '@/main/factories/http/api-url.factory'
-import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client.factory'
+
 import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
 import { RemoteLoadSurveyList } from '@/data/usecases/load-survey-list/remote-survey-list'
+import { makeAuthorizeHttpGetClientDecorator } from '../../decorators/authorize-http-get-client-decorator-factory'
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
-  return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAxiosHttpClient())
+  return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAuthorizeHttpGetClientDecorator())
 }
