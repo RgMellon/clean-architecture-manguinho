066ca72022be66337080cfcd84d729ef5f274715
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-05-14 14:38:46 -0300

    refactor: create a makeSut factory

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -2,13 +2,26 @@ import { HttpGetClientSpy } from '@/data/test'
 import faker from 'faker'
 import { RemoteLoadSurveyListResult } from './remote-load-survey-result'
 
+type SutTypes = {
+  sut: RemoteLoadSurveyListResult
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (url = faker.internet.url()) => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new RemoteLoadSurveyListResult(url, httpGetClientSpy)
+
+  return {
+    sut,
+    httpGetClientSpy
+  }
+}
+
 describe('RemoteLoadSurveyResult', () => {
   test('Should call HttpGetClient with correct URL', async () => {
     const url = faker.internet.url()
-    const httpGetClientSpy = new HttpGetClientSpy()
-    const sut = new RemoteLoadSurveyListResult(url, httpGetClientSpy)
+    const { sut, httpGetClientSpy } = makeSut(url)
     await sut.load()
-
     expect(httpGetClientSpy.url).toBe(url)
   })
 })
