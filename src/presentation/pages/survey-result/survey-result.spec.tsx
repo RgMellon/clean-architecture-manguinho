a7be0f949acf9e31a274335f151aad0a93458511
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-05-21 15:17:25 -0300

    refactor: move SUT creator to a SUT factory

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -4,14 +4,17 @@ import { render, screen } from '@testing-library/react'
 import React from 'react'
 import { SurveyResult } from '.'
 
+const makeSut = (): void => {
+  render(
+    <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
+      <SurveyResult />
+    </ApiContext.Provider>
+  )
+}
+
 describe('Survey Result Component', () => {
   test('Should present correct initial state', () => {
-    render(
-      <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
-        <SurveyResult />
-      </ApiContext.Provider>
-    )
-
+    makeSut()
     const surveyResult = screen.getByTestId('survey-result')
     expect(surveyResult.childElementCount).toBe(0)
     expect(screen.queryByTestId('error')).not.toBeInTheDocument()
