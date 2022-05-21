437a6a0747fc9db15c50952660285820d814c74e
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-05-21 14:56:13 -0300

    fix: move Calendar tests back to SurveyList

--- src/presentation/pages/survey-list/components/item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/item/survey-item.spec.tsx
@@ -12,8 +12,13 @@ describe('SurveyItem Component', () => {
   test('Should render with correct values', () => {
     const survey = mockSurveyModel()
     survey.didAnswer = true
+    survey.date = new Date('2022-04-24T00:00:00')
 
     makeSut(survey)
+
+    expect(screen.getByTestId('day')).toHaveTextContent('24')
+    expect(screen.getByTestId('month')).toHaveTextContent('abr')
+    expect(screen.getByTestId('year')).toHaveTextContent('2022')
 
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
