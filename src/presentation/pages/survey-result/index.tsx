0092e8c3dd9f41bda1d4c5188a9571ce58a43812
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-05-24 22:28:51 -0300

    feat: ensure SurveyResult present error if LoadSurveyResult fails

--- src/presentation/pages/survey-result/index.tsx
+++ src/presentation/pages/survey-result/index.tsx
@@ -8,6 +8,7 @@ import Calendar from '@/presentation/components/calendar'
 
 import { LoadSurveyResult } from '@/domain/usecases'
 import Error from '@/presentation/components/error'
+import { useErrorHandler } from '@/presentation/hooks'
 
 type Props = {
   loadSurveyResult: LoadSurveyResult
@@ -20,13 +21,17 @@ export const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
     surveyResult: null as LoadSurveyResult.Model
   })
 
+  const handleError = useErrorHandler((error: Error) => {
+    setState({ ...state, surveyResult: null, error: error.message })
+  })
+
   useEffect(() => {
     loadSurveyResult.load().then(surveyResult => {
       setState(old => ({
         ...old,
         surveyResult: surveyResult
       }))
-    })
+    }).catch(handleError)
   }, [])
 
   const reload = () => {
@@ -34,31 +39,33 @@ export const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
   }
 
   return (
-    <div className={Styles.surveyResultWrapper}>
-      <Header />
-      <div data-testid="survey-result" className={Styles.contentWrap}>
-        {state.surveyResult && <>
-          <hgroup>
-            <Calendar date={state.surveyResult.date} className={Styles.calendarWrap} />
-            <h2 data-testid="question">{state.surveyResult.question}</h2>
-          </hgroup>
-          <FlipMove data-testid="answers" className={Styles.answersList}>
-            {state.surveyResult.answers.map(answer => <li key={answer.answer} data-testid="answer-wrap" className={answer.isCurrentAccountAnswer ? Styles.active : ''}>
-              {!!answer.image && <img data-testid="image" src={answer.image} alt={answer.answer} />}
-              <span data-testid="answer" className={Styles.answer}>{answer.answer}</span>
-              <span data-testid="percent" className={Styles.percent}>{answer.percent}%</span>
-            </li>
-            )}
-          </FlipMove>
+    <>
+      <div className={Styles.surveyResultWrapper}>
+        <Header />
+        <div data-testid="survey-result" className={Styles.contentWrap}>
+          {state.surveyResult && <>
+            <hgroup>
+              <Calendar date={state.surveyResult.date} className={Styles.calendarWrap} />
+              <h2 data-testid="question">{state.surveyResult.question}</h2>
+            </hgroup>
+            <FlipMove data-testid="answers" className={Styles.answersList}>
+              {state.surveyResult.answers.map(answer => <li key={answer.answer} data-testid="answer-wrap" className={answer.isCurrentAccountAnswer ? Styles.active : ''}>
+                {!!answer.image && <img data-testid="image" src={answer.image} alt={answer.answer} />}
+                <span data-testid="answer" className={Styles.answer}>{answer.answer}</span>
+                <span data-testid="percent" className={Styles.percent}>{answer.percent}%</span>
+              </li>
+              )}
+            </FlipMove>
 
-          <button> Voltar </button>
-        </>
-        }
+            <button> Voltar </button>
+          </>
+          }
 
-        {state.isLoading && <Loading /> }
-        {state.error && <Error error={state.error} reload={reload} />}
+          {state.isLoading && <Loading /> }
+          {state.error && <Error error={state.error} reload={reload} />}
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   )
 }
